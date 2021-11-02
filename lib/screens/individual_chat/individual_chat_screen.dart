c88888074590d1e8c6411873b94ee236004d3bee
Hriday Pradhan <hridayjyotipradhandc@gmail.com>
2021-11-03 01:53:21 +0530

    Scrolling and renderflex changes

--- lib/screens/individual_chat/individual_chat_screen.dart
+++ lib/screens/individual_chat/individual_chat_screen.dart
@@ -1,129 +1,193 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:snu_connect/global/constants/colors.dart';
-import 'package:bubble/bubble.dart';
 import 'package:snu_connect/models/message.dart';
+import 'package:snu_connect/screens/individual_chat/widgets/message_bubble.dart';
 
-//TODO remove this
-String myemail = "km224";
+String myEmail = 'km224';
+final List<Message> dummyMessages = [
+  Message(
+    text: 'Hi hello much greetings to you my fellow person',
+    senderEmail: 'km224',
+    timestamp: DateTime.now(),
+  ),
+  Message(
+      text: 'Bye dear detestable human xyzabc asdjhfljf;asd;',
+      senderEmail: 'sa350',
+      timestamp: DateTime.now()),
+  Message(
+      text: 'Ok mother kill me then',
+      senderEmail: 'km224',
+      timestamp: DateTime.now()),
+  Message(
+    text: 'Hmm',
+    senderEmail: 'sa350',
+    timestamp: DateTime.now(),
+  ),
+  Message(
+    text: 'No',
+    senderEmail: 'km224',
+    timestamp: DateTime.now(),
+  ),
+  Message(
+    text: 'Why no?',
+    senderEmail: 'sa350',
+    timestamp: DateTime.now(),
+  ),
+  Message(
+      text: 'Ok mother kill me then',
+      senderEmail: 'km224',
+      timestamp: DateTime.now()),
+  Message(
+    text: 'Hmm',
+    senderEmail: 'sa350',
+    timestamp: DateTime.now(),
+  ),
+  Message(
+    text: 'No',
+    senderEmail: 'km224',
+    timestamp: DateTime.now(),
+  ),
+  Message(
+    text: 'Why no?',
+    senderEmail: 'sa350',
+    timestamp: DateTime.now(),
+  ),
+  Message(
+      text: 'Ok mother kill me then',
+      senderEmail: 'km224',
+      timestamp: DateTime.now()),
+  Message(
+    text: 'Hmm',
+    senderEmail: 'sa350',
+    timestamp: DateTime.now(),
+  ),
+  Message(
+    text: 'No',
+    senderEmail: 'km224',
+    timestamp: DateTime.now(),
+  ),
+  Message(
+    text: 'Why no?',
+    senderEmail: 'sa350',
+    timestamp: DateTime.now(),
+  ),
+  Message(
+      text: 'Ok mother kill me then',
+      senderEmail: 'km224',
+      timestamp: DateTime.now()),
+  Message(
+    text: 'Hmm',
+    senderEmail: 'sa350',
+    timestamp: DateTime.now(),
+  ),
+  Message(
+    text: 'No',
+    senderEmail: 'km224',
+    timestamp: DateTime.now(),
+  ),
+  Message(
+    text: 'Why no?',
+    senderEmail: 'sa350',
+    timestamp: DateTime.now(),
+  ),
+];
 
 class IndividualChatScreen extends StatelessWidget {
   static const id = 'individual_chat';
-  IndividualChatScreen({Key? key}) : super(key: key);
-
-  final List<Message> dummyMessages = [
-    Message(
-      text: 'Hi hello much greetings to you my fellow person',
-      senderEmail: 'km224',
-      timestamp: DateTime.now(),
-    ),
-    Message(
-        text: 'Bye dear detestable human xyzabc asdjhfljf;asd;',
-        senderEmail: 'sa350',
-        timestamp: DateTime.now()),
-    Message(
-        text: 'Ok mother kill me then',
-        senderEmail: 'km224',
-        timestamp: DateTime.now()),
-    Message(text: 'Hmm', senderEmail: 'sa350', timestamp: DateTime.now()),
-    Message(text: 'No', senderEmail: 'km224', timestamp: DateTime.now()),
-    Message(text: 'Why no?', senderEmail: 'sa350', timestamp: DateTime.now())
-  ];
+  const IndividualChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(
+                15.0,
+                15.0,
+                15.0,
+                0,
+              ),
+              child: Row(
                 children: const [
+                  // TODO Add DP
                   Icon(
                     Icons.circle,
                     size: 55,
                     color: primaryPink,
                   ),
+                  // TODO Add Name
                   Text(
                     'Friend',
-                    style: TextStyle(fontSize: 41),
+                    style: TextStyle(fontSize: 40),
                   ),
-                  SizedBox(
-                    width: 150,
-                  ),
+                  // TODO Make size dependent
+                  SizedBox(width: 150),
                 ],
               ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: lightPink,
-                ),
-                margin: const EdgeInsets.all(10.0),
-                width: size.width * 0.99,
-                height: size.height * 0.80,
+            ),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(15.0),
                 padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Expanded(
-                      child: ListView.builder(
+                decoration: BoxDecoration(
+                  color: lightPink,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      ListView.builder(
+                        dragStartBehavior: DragStartBehavior.down,
+                        physics: const NeverScrollableScrollPhysics(),
                         shrinkWrap: true,
                         itemCount: dummyMessages.length,
                         itemBuilder: (context, index) {
+                          // TODO Compare to user's email ID
                           bool isMe =
-                              dummyMessages[index].senderEmail == myemail;
-                          return Bubble(
-                            style: BubbleStyle(
-                              nip:
-                                  isMe ? BubbleNip.rightTop : BubbleNip.leftTop,
-                              color: isMe ? Colors.white : primaryPink,
-                              borderColor: isMe ? primaryPink : Colors.white,
-                              borderWidth: 2,
-                              elevation: 4,
-                              margin: BubbleEdges.only(
-                                top: 8,
-                                right: isMe ? 8 : 35,
-                                left: isMe ? 35 : 8,
-                                bottom: 8,
-                              ),
-                              padding: const BubbleEdges.all(8.0),
-                              alignment:
-                                  isMe ? Alignment.topRight : Alignment.topLeft,
-                            ),
-                            child: Text(
-                              dummyMessages[index].text,
-                              style: TextStyle(
-                                  color: isMe ? Colors.black : Colors.white),
-                            ),
+                              dummyMessages[index].senderEmail == myEmail;
+                          return MessageBubble(
+                            isMe: isMe,
+                            message: dummyMessages[index],
                           );
                         },
                       ),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: "Type something ...",
-                        hintStyle: const TextStyle(color: primaryPink),
-                        fillColor: Colors.pinkAccent,
-                        hoverColor: Colors.white,
-                        focusColor: Colors.white,
-                        enabledBorder: OutlineInputBorder(
-                          borderSide:
-                              const BorderSide(color: Colors.white, width: 3.0),
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        suffixIcon: IconButton(
-                          icon: const Icon(Icons.send),
-                          onPressed: () {},
+                      const SizedBox(height: 10.0),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: "Type a message",
+                          hintStyle: const TextStyle(color: primaryPink),
+                          fillColor: Colors.pinkAccent,
+                          hoverColor: Colors.white,
+                          focusColor: Colors.white,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.white, width: 3.0),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.white, width: 3.0),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          suffixIcon: IconButton(
+                            icon: const Icon(
+                              Icons.send,
+                              color: primaryPink,
+                            ),
+                            onPressed: () {},
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/screens/individual_chat/widgets/message_bubble.dart
@@ -0,0 +1,42 @@
+import 'package:bubble/bubble.dart';
+import 'package:flutter/material.dart';
+import 'package:snu_connect/global/constants/colors.dart';
+import 'package:snu_connect/models/message.dart';
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({
+    Key? key,
+    required this.isMe,
+    required this.message,
+  }) : super(key: key);
+
+  final bool isMe;
+  final Message message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Bubble(
+      style: BubbleStyle(
+        nip: isMe ? BubbleNip.rightTop : BubbleNip.leftTop,
+        color: isMe ? Colors.white : primaryPink,
+        borderColor: isMe ? primaryPink : Colors.white,
+        borderWidth: 2,
+        elevation: 4,
+        margin: BubbleEdges.only(
+          top: 8,
+          right: isMe ? 8 : 35,
+          left: isMe ? 35 : 8,
+          bottom: 8,
+        ),
+        padding: const BubbleEdges.all(8.0),
+        alignment: isMe ? Alignment.topRight : Alignment.topLeft,
+      ),
+      child: Text(
+        message.text,
+        style: TextStyle(
+          color: isMe ? Colors.black : Colors.white,
+        ),
+      ),
+    );
+  }
+}
