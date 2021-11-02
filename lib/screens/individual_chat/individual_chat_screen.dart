1f6fdd2c2b71f969578ae9a31ace351c8fe58938
kreetycrawly <62297087+kreetycrawly@users.noreply.github.com>
2021-11-02 22:09:32 +0530

    visual improvements

--- lib/screens/individual_chat/individual_chat_screen.dart
+++ lib/screens/individual_chat/individual_chat_screen.dart
@@ -10,10 +10,20 @@ class IndividualChatScreen extends StatelessWidget {
   static const id = 'individual_chat';
   IndividualChatScreen({Key? key}) : super(key: key);
 
-  List<Message> dummyMessages = [
-    Message(text: 'Hi', senderEmail: 'km224', timestamp: DateTime.now()),
-    Message(text: 'Bye', senderEmail: 'sa350', timestamp: DateTime.now()),
-    Message(text: 'Ok', senderEmail: 'km224', timestamp: DateTime.now()),
+  final List<Message> dummyMessages = [
+    Message(
+      text: 'Hi hello much greetings to you my fellow person',
+      senderEmail: 'km224',
+      timestamp: DateTime.now(),
+    ),
+    Message(
+        text: 'Bye dear detestable human xyzabc asdjhfljf;asd;',
+        senderEmail: 'sa350',
+        timestamp: DateTime.now()),
+    Message(
+        text: 'Ok mother kill me then',
+        senderEmail: 'km224',
+        timestamp: DateTime.now()),
     Message(text: 'Hmm', senderEmail: 'sa350', timestamp: DateTime.now()),
     Message(text: 'No', senderEmail: 'km224', timestamp: DateTime.now()),
     Message(text: 'Why no?', senderEmail: 'sa350', timestamp: DateTime.now())
@@ -54,45 +64,63 @@ class IndividualChatScreen extends StatelessWidget {
                 width: size.width * 0.99,
                 height: size.height * 0.80,
                 padding: const EdgeInsets.all(10.0),
-                child: Column(children: [
-                  Expanded(
-                    child: ListView.builder(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: ListView.builder(
+                        shrinkWrap: true,
                         itemCount: dummyMessages.length,
                         itemBuilder: (context, index) {
                           bool isMe =
                               dummyMessages[index].senderEmail == myemail;
                           return Bubble(
                             style: BubbleStyle(
-                              nip: isMe
-                                  ? BubbleNip.rightCenter
-                                  : BubbleNip.leftCenter,
+                              nip:
+                                  isMe ? BubbleNip.rightTop : BubbleNip.leftTop,
                               color: isMe ? Colors.white : primaryPink,
                               borderColor: isMe ? primaryPink : Colors.white,
                               borderWidth: 2,
                               elevation: 4,
-                              margin: const BubbleEdges.only(
+                              margin: BubbleEdges.only(
                                 top: 8,
-                                right: 8,
-                                left: 8,
+                                right: isMe ? 8 : 35,
+                                left: isMe ? 35 : 8,
+                                bottom: 8,
                               ),
+                              padding: const BubbleEdges.all(8.0),
                               alignment:
                                   isMe ? Alignment.topRight : Alignment.topLeft,
                             ),
-                            child: Text(dummyMessages[index].text),
+                            child: Text(
+                              dummyMessages[index].text,
+                              style: TextStyle(
+                                  color: isMe ? Colors.black : Colors.white),
+                            ),
                           );
-                        }),
-                  ),
-                  const TextField(
-                    decoration: InputDecoration(
-                      hintText: "Type something ...",
-                      hintStyle: TextStyle(color: primaryPink),
-                      fillColor: Colors.pinkAccent,
-                      hoverColor: Colors.white,
-                      focusColor: Colors.white,
+                        },
+                      ),
                     ),
-                  ),
-                  IconButton(icon: const Icon(Icons.send), onPressed: () {}),
-                ]),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Type something ...",
+                        hintStyle: const TextStyle(color: primaryPink),
+                        fillColor: Colors.pinkAccent,
+                        hoverColor: Colors.white,
+                        focusColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.white, width: 3.0),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.send),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
